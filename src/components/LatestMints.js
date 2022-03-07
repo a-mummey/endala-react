9d278880360544c78a3479ea037cf039edea3df2
Adrian Mummey <amummey@gmail.com>
2022-03-07 09:44:53 -0300

    Get Latest Mints

--- src/components/LatestMints.js
+++ src/components/LatestMints.js
@@ -1,11 +1,18 @@
+import { useRecoilValueLoadable } from "recoil";
+import { allMintedTokensState } from "../state";
 import MiniThumbList from "./MiniThumbList";
 
 function LatestMints() {
+  const allMintedTokens = useRecoilValueLoadable(allMintedTokensState);
+  const tokenIds = allMintedTokens.valueMaybe() || [];
+  const latestTokenIds = [...tokenIds]
+    .sort((a, b) => parseInt(b) - parseInt(a))
+    .slice(0, 100);
   return (
     <div className="container">
       <h2>Latest Mints</h2>
       <div className="grid">
-        <MiniThumbList tokenIds={[1, 2, 3]}></MiniThumbList>
+        <MiniThumbList tokenIds={latestTokenIds}></MiniThumbList>
       </div>
     </div>
   );
