8657825b001d8d41c00a2e12633e4a7cc6880576
Adrian Mummey <amummey@gmail.com>
2022-03-09 13:49:47 -0300

    Add countdown

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -2,11 +2,39 @@ import config from "../config";
 import "./Hero.css";
 import MintButton from "./MintButton";
 import ProgressBar from "./ProgressBar";
+import Countdown from "react-countdown";
 
 function Hero() {
   const randInt =
     Math.floor(Math.random() * (config.numHeroImages - 1 + 1)) + 1;
   const heroStyle = `linear-gradient(rgba(0, 0, 0, 0.9), rgba(0, 0, 0, 0.6)), url("${config.fileBase}hero/hero${randInt}.jpg"`;
+
+  const renderer = ({
+    days,
+    hours,
+    minutes,
+    seconds,
+    formatted,
+    completed,
+  }) => {
+    if (completed) {
+      // Render a completed state
+      return <MintButton></MintButton>;
+    } else {
+      const plural = (i) => (i == 1 ? "" : "s");
+      const d = days ? `${days} day${plural(days)}` : "";
+      const h = hours ? `${hours} hour${plural(hours)}` : "";
+      const m = minutes ? `${minutes} minute${plural(minutes)}` : "";
+      const s = seconds ? `${seconds} second${plural(seconds)}` : "";
+
+      // Render a countdown
+      return (
+        <button className="contrast" disabled>
+          {`${d} ${h} ${m} ${s}`}
+        </button>
+      );
+    }
+  };
 
   return (
     <div
@@ -15,16 +43,21 @@ function Hero() {
       style={{ backgroundImage: heroStyle }}
     >
       <header className="container">
-        <div>
-          <hgroup>
-            <h1>Endala</h1>
-            <h2>A Generative NFT Project on Stargaze</h2>
-          </hgroup>
-          <p>
-            <MintButton></MintButton>
-          </p>
+        <div className="row">
+          <div className="col-sm-6">
+            <hgroup>
+              <h1>Endala</h1>
+              <h2>A Generative NFT Project on Stargaze</h2>
+            </hgroup>
+            <p>
+              <Countdown
+                renderer={renderer}
+                date={"2022-03-11T21:00:00.000Z"}
+              ></Countdown>
+            </p>
+            <ProgressBar></ProgressBar>
+          </div>
         </div>
-        <ProgressBar></ProgressBar>
       </header>
     </div>
   );
--- src/components/ProgressBar.js
+++ src/components/ProgressBar.js
@@ -7,15 +7,13 @@ function ProgressBar() {
   const mintedCountVal = mintedCount === "?" ? 0 : mintedCount;
   const totalMints = config.totalNumMints;
   return (
-    <div id="mint-progress-wrap" className="container">
-      <div className="progress-header">
-        Endalas Minted: {mintedCount} / {totalMints}
-        <progress
-          id="mint-progress-bar"
-          value={mintedCountVal}
-          max={totalMints}
-        ></progress>
-      </div>
+    <div className="progress-header">
+      Endalas Minted: {mintedCount} / {totalMints}
+      <progress
+        id="mint-progress-bar"
+        value={mintedCountVal}
+        max={totalMints}
+      ></progress>
     </div>
   );
 }
